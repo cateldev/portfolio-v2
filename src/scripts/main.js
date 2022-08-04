cf0f916f0cb59f90b51bc98961c2021487991c94
Matheus Catel <catelmatt@hotmail.com>
2022-08-04 11:05:32 -0300

    Header Glassmorphism added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
+//menu animation
+
 let menu = document.querySelector(".menu-icon");
 
 menu.onclick = () => {
-    menu.classList.toggle("move");
-}
+  menu.classList.toggle("move");
+};
+
+//Header Background Change On Scroll
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("header-active", window.scrollY > 0);
+});
