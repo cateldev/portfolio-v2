0a5d26f39fb6983cb52bdc2b901801ad8ddd5390
Matheus Catel <catelmatt@hotmail.com>
2022-08-04 17:37:57 -0300

    Menu Button Scritps added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,16 @@
 //menu animation
 
 let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
+  navbar.classList.toggle("open-menu");
   menu.classList.toggle("move");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("open-menu");
+  menu.classList.remove("move");
 };
 
 //Header Background Change On Scroll
